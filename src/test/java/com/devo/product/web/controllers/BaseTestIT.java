0d300a8173b6641e4bfcd2034b92b852256286b8
denysvovk <corax123@3>
2023-04-02 17:58:14 +0300

    simple java config

--- src/test/java/com/devo/product/web/controllers/BaseTestIT.java
+++ src/test/java/com/devo/product/web/controllers/BaseTestIT.java
@@ -2,7 +2,9 @@ package com.devo.product.web.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -10,7 +12,7 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest
-public abstract class BaseIT {
+public abstract class BaseTestIT {
     @Autowired
     WebApplicationContext wac;
 
--- src/test/java/com/devo/product/web/controllers/CustomerControllerTestTestIT.java
+++ src/test/java/com/devo/product/web/controllers/CustomerControllerTestTestIT.java
@@ -6,10 +6,10 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-public class CustomerControllerIT extends BaseIT {
+public class CustomerControllerTestTestIT extends BaseTestIT {
 
     @Test
-    void listProducts_ok() throws Exception {
+    void listCustomers_ok() throws Exception {
         mockMvc.perform(get("/api/v1/customer")
                 .with(httpBasic("devo", "supersecreto")))
                 .andExpect(status().isOk());
